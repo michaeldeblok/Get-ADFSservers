7b30ba8eed3cdc0c1af2380bf456d22c774d64de
michaeldeblok <michaeldeblok@gmail.com>
2016-07-01 22:38:33 -0700

    Get-ADFSdiag will find all ADFS servers and test them
    Get-ADFSdiag will find all your on-premise ADFS servers run diagnostic tests against them, auto-remediate some issues and output everything into a file for you

---
+++ Get-ADFSdiag.ps1
@@ -0,0 +1,65 @@
+#Requires –Version 4
+#Requires -RunAsAdministrator 
+
+$cred = Get-Credential
+$adfs = Read-Host -Prompt 'Please type in your adfs endpoint hostname (i.e. adfs.contoso.com)' 
+
+$formatenumerationlimit = -1
+$dc = $env:Logonserver -replace "\\", ""
+$s = New-PSSession -ComputerName $dc -Credential $cred
+Import-PSSession -Session $s -Module activedirectory -Prefix dc
+$services = "adfssrv","MSSQL$MICROSOFT%"
+$servers = Get-dcADComputer -LDAPFilter "(&(objectcategory=computer)(OperatingSystem=*server*))"
+$adfsservers = ForEach-Object {Get-WmiObject Win32_Service -ComputerName $servers.dnshostname -Filter "Name Like 'adfssrv'" -Credential $cred | select-object PSComputerName -ExpandProperty PSComputerName}
+$ips = $adfsservers | foreach {Resolve-DNSName $_ | Select-Object IPAddress -ExpandProperty IPAddress}
+$adfssrvs = $adfsservers | ForEach {Get-dcADComputer "$_" | Select-Object DnsHostName -ExpandProperty DnsHostName}
+$adfssessions = New-PSSession -ComputerName $adfssrvs -Credential $cred
+Start-TranScript c:\temp\ADFS-TESTS.TXT -force
+write-host ====================================
+Write-host Number of ADFS Servers: ($adfsservers).count
+$adfssrvs
+write-host ====================================
+Write-Host -foregroundcolor "Green" Showing status of services on ADFS Servers:
+$services | ForEach-Object {Get-WmiObject Win32_Service -ComputerName $servers.dnshostname -Filter "Name Like '$_'" -Credential $cred | Format-Table Name, DisplayName, State, StartMode, StartName, SystemName -auto}
+
+$f = "c:\temp\ADFSDiagnostics.psm1"
+$c = Get-Content $f
+icm -Session $adfssessions -ScriptBlock {mkdir c:\temp -force -erroraction silentlycontinue}
+icm -Session $adfssessions -ScriptBlock {param($filename,$contents) `
+Set-Content -Path $filename -Value $contents} -ArgumentList $f,$c
+
+icm -Session $adfssessions -ScriptBlock {
+C:
+cd \temp
+Import-Module .\ADFSDiagnostics.psm1
+Write-Host -foregroundcolor "Green" Running cmdlet Get-AdfsSystemInformation on $env:computername
+Get-AdfsSystemInformation
+Write-Host -foregroundcolor "Green" Running cmdlet Get-AdfsServerConfiguration on $env:computername
+Get-AdfsServerConfiguration
+Write-Host -foregroundcolor "Green" Running cmdlet Test-AdfsServerHealth on $env:computername
+Test-AdfsServerHealth | ft Name,Result -AutoSize
+Write-Host -foregroundcolor "Green" Running cmdlet Test-AdfsServerHealth showing failures on $env:computername
+Test-AdfsServerHealth | where {$_.Result -eq "Fail"} | fl
+}
+
+C:
+cd \temp
+Import-Module .\ADFSDiagnostics.psm1
+Import-Module .\Hostnames.psm1
+foreach ($ip in $ips)
+{
+Add-Hostnames $ip $adfs
+Test-AdfsServerToken -federationServer $adfs -appliesTo urn:federation:MicrosoftOnline
+Test-AdfsServerToken -federationServer $adfs -appliesTo urn:federation:MicrosoftOnline -credential $cred
+$token = [Xml](Test-AdfsServerToken -federationServer $adfs -appliesTo urn:federation:MicrosoftOnline)
+$token.Envelope.Body.RequestSecurityTokenResponse.RequestedSecurityToken.Assertion.AttributeStatement.Attribute | ft
+Remove-Hostnames $adfs
+}
+Stop-TranScript
+
+
+Write-Host -foregroundcolor "Green" Starting services marked as AUTO that are now marked as STOPPED
+icm -Session $adfssessions -ScriptBlock {get-wmiobject win32_service | where-object {$_.Startmode -eq "auto" -and $_.State -ne "running"}| Start-Service -Verbose}
+
+
+Get-PSSession | Remove-PSSession
